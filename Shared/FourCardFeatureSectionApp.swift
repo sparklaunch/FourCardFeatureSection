38ebc5bd849d7b545a6d95f1eff65dcf7c75e965
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-31 07:46:39 +0900

    Initial Commit

---
+++ Shared/FourCardFeatureSectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourCardFeatureSectionApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/31.
+//
+
+import SwiftUI
+
+@main
+struct FourCardFeatureSectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
