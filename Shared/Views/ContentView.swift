6504f7a0ba496dc2b73430f85b1f1b81195dd1db
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-31 07:58:08 +0900

    Add the background.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color("BackgroundColor")
+            ScrollView {
+                Title()
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
---
+++ Shared/Views/Title.swift
@@ -0,0 +1,16 @@
+//
+//  Title.swift
+//  FourCardFeatureSection (iOS)
+//
+//  Created by Jinwook Kim on 2022/03/31.
+//
+
+import SwiftUI
+
+struct Title: View {
+    var body: some View {
+        VStack {
+
+        }
+    }
+}
